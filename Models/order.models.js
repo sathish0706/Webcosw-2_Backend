0eaf2f157d40cc1c015af61682c062a0930d035f
Sathish R <107674282+sathish0706@users.noreply.github.com>
2023-03-22 07:01:14 +0000

    Update and rename order.route.js to order.models.js

--- Models/order.models.js
+++ Models/order.models.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema(
   {
     name: { type: String, require },
-    email: { type: String, require, unique: true },
+    email: { type: String, require, unique: false },
     address: { type: String, require },
   },
   {
